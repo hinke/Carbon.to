89f90aaa2cc8b0ad039387cb4bc183d744a8a624
Henrik Berggren <hinke@hinkeb.com>
2009-09-12 01:14:37 +0200

    API v0.1

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,13 +1,40 @@
 class ConversionController < ApplicationController
   
-  def show
-    c = Conversion.find_by_slug(params[:id])
+  def index
+    all = Conversion.all
+    
+    all.map {|c| c.amount = co2}
     
     respond_to do |format|
-      format.xml {render :xml => c.to_xml  }
+      format.xml {render :xml => all.to_xml(:skip_types => true)  }
+      format.json {render :json => all.to_json  }
+    end
+    
+  end
+  
+  def show
+    
+    c = Conversion.find_by_slug(params[:id])
+    
+    c.amount = co2
+    
+    respond_to do |format|
+      format.xml {render :xml => c.to_xml(:skip_types => true)  }
       format.json {render :json => c.to_json  }
     end
     
+  end
+
+  private
+  
+  def co2
+    if !params[:co2].nil?
+      co2 = params[:co2].to_i
+      if co2 < 1 then co2 = 1 end
+    else
+      co2 = 1
+    end
+    return co2
   end
   
 end
--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,13 @@
 class Conversion < ActiveRecord::Base
   belongs_to :category
+  attr_accessor :amount
+  
+  def amount=(co2)
+    write_attribute(:amount, (co2/self.carbon).ceil)
+  end
+
+  def amount
+    read_attribute(:amount).to_i
+  end
+  
 end
