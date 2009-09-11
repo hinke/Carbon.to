710e95638355b18b493934e49979dc77b937155b
Henrik Berggren <hinke@hinkeb.com>
2009-09-11 23:13:08 +0200

    API

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,2 +1,13 @@
 class ConversionController < ApplicationController
+  
+  def show
+    c = Conversion.find_by_slug(params[:id])
+    
+    respond_to do |format|
+      format.xml {render :xml => c.to_xml  }
+      format.json {render :json => c.to_json  }
+    end
+    
+  end
+  
 end
