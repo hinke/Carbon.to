7d3d94cc76195ba576a2981dd394d88770725aff
Henrik Berggren <hinke@hinkeb.com>
2009-09-12 10:58:48 +0200

    Fixed wrong amount

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,8 @@ class WelcomeController < ApplicationController
     @all.map {|c| c.amount = co2}
     
     @co2 = Conversion.find(:first, :conditions => {:slug => "co2"})
+    @co2.amount = co2
+    
     @random = @all.rand
   end
 
@@ -16,6 +18,7 @@ class WelcomeController < ApplicationController
   end
   
   private
+  
   def co2
     if !params[:co2].nil?
       co2 = params[:co2].to_i
